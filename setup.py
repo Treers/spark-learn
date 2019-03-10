0dbe86d73ec7c2c241f551fd4b23cf79af373e09
Youcai Fang <ahutfangyoucai@163.com>
2019-03-11 00:02:21 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+import sys
+from setuptools import setup,find_packages
+
+setup(
+    name='spark-scorecard',
+    version='0.1.0',
+    #url='',
+    license='MIT',
+    author='Youcai Fang',
+    author_email='ahutfangyoucai@163.com',
+    description=__doc__,
+    keywords="spark-scorecard",
+    packages=find_packages(),
+    #scripts=[],
+    #include_package_data=True,
+    zip_safe=False,
+    platforms='any',
+    install_requires=['numpy', 'pandas','pyspark','scikit-learn',"ipywidgets >= 7.0.0"]
+)
