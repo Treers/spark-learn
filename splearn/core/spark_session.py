4dc30b5c5c7f11763667b12f5f564b5458833aef
Youcai Fang <ahutfangyoucai@163.com>
2019-04-25 11:29:10 +0800

    Add files via upload

---
+++ splearn/core/spark_session.py
@@ -0,0 +1,38 @@
+import os
+import sys
+
+class SparkSessionNow(object):
+    '''
+    spark = SparkSessionNow(is_remote=True) \
+    		.spark_entry(appName='myapp',spark_home='/usr/local/spark')
+    '''
+    def __init__(self, is_remote=False):
+        self.is_remote = is_remote
+
+    def _remote_environ(self, spark_home):
+        if isinstance("spark_home", str):
+            sys_path = spark_home + '/python'
+            os.environ['SPARK_HOME'] = spark_home
+            sys.path.append(sys_path)
+
+    def spark_entry(self, appName, config={'key':"spark.some.config.option",'value':"some-value"}, 
+    				spark_home=None, master='local'):
+
+        if self.is_remote:
+            self._remote_environ(spark_home)
+
+        from pyspark.sql import SparkSession
+        spark = SparkSession.builder \
+            .master(master) \
+            .appName(appName) \
+            .config(config['key'], config['value']) \
+            .getOrCreate()
+
+        return spark
+
+
+
+
+
+
+
