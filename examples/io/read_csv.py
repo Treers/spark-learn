57974dde7ccf3774398055108c122515b502d7e9
Youcai Fang <ahutfangyoucai@163.com>
2019-04-28 15:43:57 +0800

    Update read_csv.py

--- examples/io/read_csv.py
+++ examples/io/read_csv.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 import sys
 sys.path.append('/tmp/pycharm_project_920/spark-learn/')
-from splearn.io import load_data
+from splearn.io import csv_txt
 
-data=load_data.LoadFromLocal('/tmp/learningPySpark/Chapter03/flight-data/departuredelays.csv',spark_home='/usr/local/spark')
+data=load_data.read_csv('/tmp/learningPySpark/Chapter03/flight-data/departuredelays.csv',spark_home='/usr/local/spark')
 
 data.data2frame(header='True',inferSchema='True',sep=',').show()
