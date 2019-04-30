9362521501982551e78e016ff5564822185faf86
youcai <ahutfangyoucai@163.com>
2019-04-30 14:04:09 +0800

    2019-04-30

--- splearn/io/files.py
+++ splearn/io/files.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
 
-from ..utils._sessions import SparkSessionNow
+from ..utils._sessions import SessionInit
 from ..utils._discriminators import is_path
-from .. import config
 
 
-class ReadCsv(object):
+class ReadFile(SessionInit):
     """Interface used to load a DataFrame from external .csv or .txt files
 
     :param file_path: The path of a file. Whether it is a local file or an HDFS file, the
@@ -13,10 +12,10 @@ class ReadCsv(object):
     :param hdfs: If True, the file will be read from HDFS, otherwise it will be read from
         the local file system.
 
-    >>> read_csv =
+    >>>
     """
-    def __init__(self, file_path, hdfs=False):
 
+    def __init__(self, file_path, hdfs=False):
         if is_path(file_path):
             if not hdfs:
                 self.file_path = 'file://' + file_path
@@ -25,16 +24,13 @@ class ReadCsv(object):
         else:
             raise NotADirectoryError("The path may look like '/usr/local/xxx.csv'")
 
-        self.spark = SparkSessionNow(spark_home=config.get('spark_home', default=None)) \
-            .spark_entry(config.get('app_name'),
-                         config.get('spark_config', default=['', '', None]),
-                         config.get('master'))
+        super(ReadFile, self).__init__()
 
-    def to_frame(self, schema=None, sep=None, encoding=None, quote=None, escape=None,
-                 comment=None, header=None, inferSchema=None, ignoreLeadingWhiteSpace=None,
-                 ignoreTrailingWhiteSpace=None, nullValue=None, nanValue=None, positiveInf=None,
-                 negativeInf=None, dateFormat=None, timestampFormat=None, maxColumns=None,
-                 maxCharsPerColumn=None, maxMalformedLogPerPartition=None, mode=None):
+    def csv2frame(self, schema=None, sep=None, encoding=None, quote=None, escape=None,
+                  comment=None, header=None, inferSchema=None, ignoreLeadingWhiteSpace=None,
+                  ignoreTrailingWhiteSpace=None, nullValue=None, nanValue=None, positiveInf=None,
+                  negativeInf=None, dateFormat=None, timestampFormat=None, maxColumns=None,
+                  maxCharsPerColumn=None, maxMalformedLogPerPartition=None, mode=None):
 
         df = self.spark.read.csv(self.file_path, schema, sep, encoding, quote, escape,
                                  comment, header, inferSchema, ignoreLeadingWhiteSpace,
@@ -42,3 +38,5 @@ class ReadCsv(object):
                                  negativeInf, dateFormat, timestampFormat, maxColumns,
                                  maxCharsPerColumn, maxMalformedLogPerPartition, mode)
         return df
+
+
--- splearn/utils/_sessions.py
+++ splearn/utils/_sessions.py
@@ -1,31 +1,39 @@
 import os
 import sys
+from .. import configuration
 
 
-class SparkSessionNow(object):
+class SessionInit(object):
     '''
     note
     '''
 
-    def __init__(self, spark_home):
-        self.spark_home = spark_home
+    def __init__(self):
 
-    def _remote_environ(self):
-        if isinstance(self.spark_home, str):
-            sys_path = self.spark_home + '/python'
-            os.environ['SPARK_HOME'] = self.spark_home
+        self.spark = self.spark_entry(configuration.get('app_name'),
+                                      configuration.get('spark_config', default=['', '', None]),
+                                      configuration.get('master'))
+
+    @staticmethod
+    def _remote_environ(spark_home):
+        if isinstance(spark_home, str):
+            sys_path = spark_home + '/python'
+            os.environ['SPARK_HOME'] = spark_home
             sys.path.append(sys_path)
 
-    def spark_entry(self, appName, config, master):
+    def spark_entry(self, app_name, spark_config, master):
 
-        if self.spark_home is not None:
-            self._remote_environ()
+        spark_home = configuration.get('spark_home', default=None)
+
+        if spark_home is not None:
+            self._remote_environ(spark_home)
 
         from pyspark.sql import SparkSession
         spark = SparkSession.builder \
             .master(master) \
-            .appName(appName) \
-            .config(config[0], config[1], config[2]) \
+            .appName(app_name) \
+            .config(spark_config[0], spark_config[1], spark_config[2]) \
             .getOrCreate()
 
         return spark
+
